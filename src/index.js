8aec1f91b8d1e4d8dbbdf224359524854ae91306
Lidia Tabędzka <lidia.tabedzka@gmail.com>
2018-01-24 11:02:07 +0100

    Add redux actions and reducer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'));
+render(
+    <Provider store={store}>
+        <h1>Inicjalizacja projektu</h1>
+    </Provider>,
+    document.getElementById('root')
+);
