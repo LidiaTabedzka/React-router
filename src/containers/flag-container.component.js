e7e5e8206ce572b2f914483edfeabb2e642b01bc
Lidia Tabędzka <lidia.tabedzka@gmail.com>
2018-01-25 18:23:41 +0100

    Add pagination

--- src/containers/flag-container.component.js
+++ src/containers/flag-container.component.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CountryFlagList from '../presentational/flag-list.component';
-import { getCountries, searchCountries, deleteCountry } from '../actions/actions-countries';
+import Pagination from '../presentational/pagination-component';
+import { getCountries, searchCountries, deleteCountry, setCountriesPerPage, setCurrentPage } from '../actions/actions-countries';
 
 class CountryFlagContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pagesRange: [1, 2, 3],
+            start_offset: 0
+        };
+    }
 
     componentDidMount() {
+        this.props.dispatch(setCountriesPerPage(5));
         this.props.dispatch(searchCountries(''));
         this.props.dispatch(getCountries());
     }
@@ -18,13 +27,44 @@ class CountryFlagContainer extends Component {
         this.props.dispatch(deleteCountry(id));
     }
 
+    chooseCountriesPerPage(event) {
+        this.props.dispatch(setCountriesPerPage(event.target.value));
+        this.createArray(event.target.value);
+    }
+
+    createArray(perPage) {
+        const pagesArray = [];
+        for (let i = 1; i <= Math.ceil(this.props.visibleCountries.length / perPage); i++) {
+            pagesArray.push(i);
+        }
+        this.setState({pagesRange : pagesArray});
+    }
+
+    currentPageHandler(number) {
+        const start_offset = (number - 1) * this.props.countriesPerPage;
+
+        this.props.dispatch(setCurrentPage(number));
+        this.setState({start_offset});
+    }
+
     render() {
         return (
             <div>
                 <div className="search text-center">
                     <input type="text" placeholder="SZUKAJ KRAJU" onChange={(e) => this.search(e)}/>
                 </div>
-                <CountryFlagList countries={this.props.visibleCountries} deleteCountry={(id) => this.deleteCountry(id)}/>
+                <div>
+                    Pokaż: 
+                    <select className="per-page-select" onChange={e => this.chooseCountriesPerPage(e)}>
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="15">15</option>
+                        <option value="20">20</option>
+                    </select>
+                    państw na stronie
+                </div>
+                <Pagination pagesRange={this.state.pagesRange} currentPage={this.props.currentPage} currentPageHandler={(number) => this.currentPageHandler(number)}/>
+                <CountryFlagList start_offset={this.state.start_offset} countriesPerPage={this.props.countriesPerPage} countries={this.props.visibleCountries} deleteCountry={(id) => this.deleteCountry(id)}/>
             </div>
         )
     }
@@ -33,7 +73,9 @@ class CountryFlagContainer extends Component {
 const mapStateToProps = function (store) {
     return {
         countries: store.countriesReducer.countries,
-        visibleCountries: store.countriesReducer.visibleCountries
+        visibleCountries: store.countriesReducer.visibleCountries,
+        countriesPerPage: store.countriesReducer.countriesPerPage,
+        currentPage: store.countriesReducer.currentPage
     };
 };
 
