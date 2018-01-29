e666c7c7ea9ce4075aaf01bf9b940e7e49802c38
Lidia Tabędzka <lidia.tabedzka@gmail.com>
2018-01-29 10:30:10 +0100

    Add gh-pages

--- src/presentational/flag.component.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CountryFlag = (props) => (
-    <div className="country-logo-wrapper">
-        <img className="country-logo" src={props.country.imageUrl} alt="country flag" />
-    </div>
-);
-
-export default CountryFlag;
